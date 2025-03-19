8b44650a28652621210c5fbb8678ed92308c9c95
slimafilipe <limafilipe.coding@gmail.com>
2025-03-19 01:05:51 -0300

    Initial commit

---
+++ src/test/java/dev/filipe/DevWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.filipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
