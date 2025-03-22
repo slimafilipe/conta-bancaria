a18c327f5b36c5cbd4ff37ad7493bf69e5a828ad
slimafilipe <limafilipe.coding@gmail.com>
2025-03-21 22:49:59 -0300

    Persistencia de dados na classe Account

--- src/main/java/dev/filipe/domain/model/Account.java
+++ src/main/java/dev/filipe/domain/model/Account.java
@@ -1,13 +1,25 @@
 package dev.filipe.domain.model;
 
+import jakarta.persistence.*;
+
 import java.math.BigDecimal;
 
+@Entity(name = "tb_account")
 public class Account {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column
     private String number;
+
     private String agency;
+
+    @Column( scale = 13, precision = 2)
     private BigDecimal balance;
+
+    @Column( scale = 13, precision = 2)
     private BigDecimal limit;
 
     public Long getId() {
